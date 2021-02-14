9d9e96580f79555f89dc28cc1c91f43ad74409f5
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-13 20:07:52 -0500

    Creando archivo clientes.json.ts con lista de objetos tipo clientes para poblar el arreglo

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{Cliente} from './cliente';
+import{CLIENTES} from './clientes.json';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -7,21 +8,11 @@ import{Cliente} from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-clientes: Cliente[]=[
-  {id:1, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:2, nombre:'Javier', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:3, nombre:'Camilo', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:4, nombre:'Gabriela', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:5, nombre:'Pablos', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:6, nombre:'Palomino', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:7, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:8, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:9, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
-  {id:10, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' }
-];
+clientes: Cliente[];
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import{ Cliente } from './cliente';
+
+export const CLIENTES: Cliente[]=[
+  {id:1, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:2, nombre:'Javier', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:3, nombre:'Camilo', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:4, nombre:'Gabriela', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:5, nombre:'Pablos', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:6, nombre:'Palomino', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:7, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:8, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:9, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' },
+  {id:10, nombre:'Andres', apellido:'Moncada', email:'javier@ufps.edu.co', createAt:'2021-20-08' }
+];
