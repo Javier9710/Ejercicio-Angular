86d3758bbdb0d4153c4d91a02cd71617acb55619
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-13 21:11:47 -0500

    Implementando navegacion y rutas en angular sin recargar pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import{ HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
+import { HeaderComponent} from './header/header.component';
+import { FooterComponent} from './footer/footer.component';
 import { DirecctivaComponent } from './direcctiva/direcctiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
-import {ClienteService} from './clientes/cliente.service';
+import { ClienteService} from './clientes/cliente.service';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirecctivaComponent},
+  {path: 'clientes', component: ClientesComponent},
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +24,8 @@ import {ClienteService} from './clientes/cliente.service';
     ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
