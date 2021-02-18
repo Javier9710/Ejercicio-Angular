9ead24edc70cbbff1ba6d4f4d42e5eb32d86de68
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-17 22:47:45 -0500

    Añadiendo eliminar cliente (crud de clientes completo)

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
-
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -18,4 +18,44 @@ clientes: Cliente[];
   );
   }
 
+  delete(cliente: Cliente): void{
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Estas Seguro?',
+      text: `Esta seguro de Eliminar al Cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, Eliminar!',
+      cancelButtonText: 'No, Cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response=>{
+            this.clientes = this.clientes.filter(cli => cli !==cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente Eliminado!',
+              'El cliente se ha eliminado con exito',
+              'success'
+            )
+
+          }
+        )
+
+
+
+
+
+
+  }
+
+})
+}
 }
