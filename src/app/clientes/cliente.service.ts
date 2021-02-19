14d9f76b0191ff063707a4a51fa48544dd247e08
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-18 19:20:31 -0500

    Manejo de error desde el frontend (obtener por id)

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
-import {of,Observable} from 'rxjs';
+import {of,Observable, throwError} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {map, catchError} from 'rxjs/operators';
+import swal from 'sweetalert2';
+
+import {Router} from '@angular/router';
 
 
 @Injectable({
@@ -14,7 +17,7 @@ export class ClienteService {
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
   private httpHeaders = new HttpHeaders({'content-type': 'application/json'})
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router:Router) {
    }
 
   getClientes():Observable<Cliente[]>{
@@ -31,7 +34,15 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes'])
+        console.log(e.error.mensaje);
+        swal.fire('Error al editar', e.error.mensaje, 'error');
+        return throwError(e);
+
+      })
+    );
 
   }
 
